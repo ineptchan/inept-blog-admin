02de9bfe24ab774f5fe6b2c9355dadf51d4049a0
ineptchan <ineptchan@outlook.com>
2026-03-17 07:51:39 +0800

    fix: userStore 添加兼容方法

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -13,5 +13,25 @@ export const useUserStore = defineStore('userStore', () => {
         profile.value = v
     }
 
-    return {profile, accessToken, saveAccessToken, saveProfile}
+    const saveUser = (v: {
+        id: number
+        nickname: string
+        username: string
+        email?: string
+        status?: boolean
+        roles?: RoleType[]
+        permissionCodes?: string[]
+    }) => {
+        profile.value = {
+            id: v.id,
+            nickname: v.nickname,
+            username: v.username,
+            email: v.email,
+            status: v.status ?? true,
+            roles: v.roles ?? [],
+            permissionCodes: v.permissionCodes ?? [],
+        }
+    }
+
+    return {profile, accessToken, saveAccessToken, saveProfile, saveUser}
 })
